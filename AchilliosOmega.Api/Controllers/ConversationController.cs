258bfab598aae3d94f2d4497f65423cef9098ce7
Dan Riddell <bobland1@hotmail.co.ukdotnet>
2021-07-18 15:10:59 +0100

    Add project files.

---
+++ AchilliosOmega.Api/Controllers/ConversationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AchilliosOmega.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConversationController : ControllerBase
+    {
+        private readonly ILogger<ConversationController> _logger;
+
+        public ConversationController(ILogger<ConversationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return string.Empty;
+        }
+    }
+}
